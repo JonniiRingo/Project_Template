6035713b6ac70e54e0e35ab16fd0a81c19376d44
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-18 12:46:44 -0700

    addition of some extra notes

--- react-setups/build.sh
+++ react-setups/build.sh
@@ -1,1 +1,7 @@
+# Change directory into the bash script folder and run this command in the terminal
+# To prepare the script file can be compiled from inside a react project folder
+
+# Be sure to change your the github username to your own user name. 
+# Go through 
+
 chmod +x setup.sh
