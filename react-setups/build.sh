685746ecb93a6345b2a7bef423579c632adffe6b
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-18 19:12:46 -0700

    update and bug fixes

--- react-setups/build.sh
+++ react-setups/build.sh
@@ -2,6 +2,7 @@
 # To prepare the script file can be compiled from inside a react project folder
 
 # Be sure to change your the github username to your own user name. 
-# Go through 
+# Go through the read me and first download all the tools and sdk's in order for the scrip to work.
+
 
 chmod +x setup.sh
